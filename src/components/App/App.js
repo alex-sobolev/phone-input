f4a5f79cbf8aa5436aab327fd1e3aaaa9f9495ab
Alex Sobolev <alex.sobolev1@gmail.com>
2022-01-31 00:54:39 +0100

    Extract helper functions

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,53 +4,53 @@ import { useState } from 'react';
 import { TextField, Box } from '@mui/material';
 import './App.css';
 
-export const App = () => {
-  const [phoneNumber, setPhoneNumber] = useState('');
+const isValid = str => {
+  if (str === '') return true;
 
-  const isValid = str => {
-    if (str === '') return true;
+  const numVal = parseInt(str[str.length - 1]);
 
-    const numVal = parseInt(str[str.length - 1]);
+  return !Number.isNaN(numVal);
+};
 
-    return !Number.isNaN(numVal);
-  };
+const formatStr = str => {
+  const len = str.length;
+  let result = '';
 
-  const formatStr = str => {
-    const len = str.length;
-    let result = '';
+  if (len > 0) {
+    const numStr = str
+      .split('')
+      .filter(char => !Number.isNaN(parseInt(char)))
+      .join('');
 
-    if (len > 0) {
-      const numStr = str
-        .split('')
-        .filter(char => !Number.isNaN(parseInt(char)))
-        .join('');
+    let areaCode = '';
+    let phoneNumber = '';
 
-      let areaCode = '';
-      let phoneNumber = '';
-
-      if (numStr.length === 0) {
-        return '';
-      }
-
-      if (numStr.length < 4) {
-        areaCode = `(${numStr}`;
-      } else {
-        areaCode = `(${numStr.slice(0, 3)}) `;
-
-        if (numStr.length < 7) {
-          phoneNumber = numStr.slice(3);
-        } else {
-          phoneNumber = `${numStr.slice(3, 6)}-${numStr.slice(6)}`;
-        }
-      }
-
-      result = `${areaCode}${phoneNumber}`;
-    } else {
-      return str;
+    if (numStr.length === 0) {
+      return '';
     }
 
-    return result;
-  };
+    if (numStr.length < 4) {
+      areaCode = `(${numStr}`;
+    } else {
+      areaCode = `(${numStr.slice(0, 3)}) `;
+
+      if (numStr.length < 7) {
+        phoneNumber = numStr.slice(3);
+      } else {
+        phoneNumber = `${numStr.slice(3, 6)}-${numStr.slice(6)}`;
+      }
+    }
+
+    result = `${areaCode}${phoneNumber}`;
+  } else {
+    return str;
+  }
+
+  return result;
+};
+
+export const App = () => {
+  const [phoneNumber, setPhoneNumber] = useState('');
 
   const onInputTyping = event => {
     const val = event.target.value;
